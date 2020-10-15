760fee227dd32f6753fc5b4000192e512a74f1c9
jivanamar <amarkumarkgp@gmail.com>
2020-10-15 08:30:09 +0530

    update

---
+++ BlogWebsite/wsgi.py
@@ -0,0 +1,11 @@
+import os
+from application import create_app
+
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+app = create_app(os.path.join(base_dir, 'settings.py'))
+
+
+if __name__ == "__main__":
+    app.run()
